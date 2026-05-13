6c0ab07d1cb130a6afa586f1420dc8e807852789
omargibreel <omarosama1124@gmail.com>
2026-05-13 07:20:13 +0300

    01-ProductModule [Entities Building]

---
+++ Talabat.Domain/Models/ProductModule/ProductBrand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Talabat.Domain.Models.ProductModule
+{
+    public class ProductBrand : BaseEntity<int>
+    {
+        public string Name { get; set; } = default!;
+    }
+}

0ce76ab505dc41b476fffac6ce1d221cb6d9c82b
omargibreel <omarosama1124@gmail.com>
2026-05-14 02:14:35 +0300

    02- Product Module Cofigurations - Update Database

---
+++ Talabat.Persistence/Data/Context/StoreDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Talabat.Domain.Models.ProductModule;
+
+namespace Talabat.Persistence.Data.Context
+{
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options)
+        {
+
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(StoreDbContext).Assembly);
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+    }
+}
